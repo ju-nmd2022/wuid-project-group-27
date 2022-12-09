6b18894e3dfc9424a289e7e7d361efe8cb23cffc
hilmanystrom <nyhi22bc@student.ju.se>
2022-12-09 11:40:11 +0100

    final work

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 
 const orderButton = document.getElementById("order")
 const orderDetails = document.getElementById("orderDetails")
-// const payButton = document.getElementById("payButton")
-// const finishPage = document.getElementById("finishPage")
+const emailAdress = document.getElementById("emailAdress")
+const number = document.getElementById("number")
+const exDate = document.getElementById("exDate")
+const cvc = document.getElementById("cvc")
+const cardHolder = document.getElementById("cardHolder")
+const street = document.getElementById("street")
+const city = document.getElementById("city")
+const zipCode = document.getElementById("zipCode")
+const PayButton = document.getElementById("payButton")
+const errorMesssage = document.getElementById("errorMessage")
+
+
+
 
 let hasOrderDetails = false;
 let hasOrderButton = false;
 hideOrderDetails();
 showOrderButton();
-// hideFinishPage();
 
-// let hasPayButton = false;
-// hideFinishPage();
 
 
 orderButton.addEventListener("click", function() {
@@ -37,24 +45,6 @@ orderDetails.addEventListener("click", function() {
  }
 })
 
-// payButton.addEventListener("click", function() {
-//     if (!hasPayButton) {
-//     hasPayButton = false;
-//    showOrderDetails();
-//    hideFinishPage();
-//     }
-// })
-
-// payButton.addEventListener("click", function() {
-//     if (!hasPayButton) {
-//     hasPayButton = true;
-//     showFinishPage();
-//     hideOrderDetails();
-//     }
-// })
-
-
-
 
 function hideOrderDetails() {
     orderDetails.hidden = true;
@@ -72,18 +62,47 @@ function showOrderButton() {
     orderButton.hidden = false;
 }
 
-// function hideFinishPage() {
-//     finishPage.hidden = true;
+// document.querySelectorAll("payButton").forEach(item => {
+//     item.addEventListener("click", PayButton => {
+//         recipe = item.value
+//     })
+// })
+
+// function checkInput() {
+//  if (emailAdress.value == "" || number.value == "" || exDate.value == "" || cvc.value == "" || 
+//  cardHolder.value == "" || street.value == "" || city.value == "" || zipCode.value == "" || cvc.value == "" ) {
+// errorMesssage.innerHTML = "bajs"
+//  }else{
+//     errorMesssage.innerHTML = ""
+//  }
 // }
 
-// function showFinishPage() {
-//     finishPage.hidden = false;
+// function checkoutPage() {
+// PayButton.addEventListener("click", checkInput)
 // }
 
 // function hidePayButton() {
-//     payButton.hidden = true;
+//     PayButton.hidden = true;
 // }
 
 // function showPayButton() {
-//     payButton.hidden = false;
+//     PayButton.hidden = false;
 // }
+
+// function hideEmailAdress() {
+//     emailAdress.hidden = true;
+// }
+
+// function showEmailAdress() {
+//     emailAdress.hidden = false;
+// }
+
+// emailAdress.addEventListener("click", function() {
+// if (!hasEmailAdress) {
+//     showPayButton();
+//     showOrderDetails();
+// }
+// }
+
+
+
